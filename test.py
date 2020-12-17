b4789d7c4e9ec33ea3cf2bdfe29793dfd0dc4bb3
Hiroki Tanaka <hiroki.tnk@gmail.com>
2020-12-17 23:12:17 +0900

    commit

--- test.py
+++ test.py
@@ -1,2 +1,19 @@
-print("hello world")
-print("changed")
+n = int(input())
+list_a = list(map(int,input().split(" ")))
+
+def div2(args):
+    return (args / 2)
+
+num_div = 0
+isOdd = False
+
+while True:
+    for i in list_a:
+        if int(i % 2) == 1:
+            isOdd = True
+    if isOdd:
+        break
+    num_div += 1
+    list_a = list(map(div2,list_a))
+
+print(num_div)
---
+++ test2.py
@@ -0,0 +1,13 @@
+N,A,B = map(int,input().split())
+
+total = 0
+for i in range(N,0,-1):
+    digits = str(i)
+    sum_digits = 0
+    #print("digits " + str(digits))
+    for j in range(len(digits)):
+        sum_digits += int(digits[j])
+    #print("sum_digits " + str(sum_digits))
+    if A <= sum_digits and sum_digits <= B:
+        total += i
+print(total)
