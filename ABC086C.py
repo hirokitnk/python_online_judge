3cb97459673da2060b5182dbe290675d122faf09
Hiroki Tanaka <hiroki.tnk@gmail.com>
2020-12-23 21:34:52 +0900

    revised

--- ABC086C.py
+++ ABC086C.py
@@ -1,21 +1,23 @@
-t[0],x[0],y[0] = 0,0,0
+t,x,y = [0],[0],[0]
 
 N = int(input())
 
 reached = True
 
-for i in range(N):
-    t[i],x[i],y[i] = map(int,input().split())
+for i in range(1,N+1):
+    args = list(map(int,input().split(" ")))
+    t.append(args[0])
+    x.append(args[1])
+    y.append(args[2])
+
     deltaTime = t[i] - t[i-1]
     deltaDist = (x[i]- x[i-1]) + (y[i]-y[i-1])
 
     if deltaDist > deltaTime:
         reached = False
-        break
 
-    if deltaDist % 2 != deltaTiem % 2:
+    if deltaDist % 2 != deltaTime % 2:
         reached = False
-        break
 
 if reached:
     print("Yes")
