481e3529c583084a67ffce13955deaec2e789e8e
Hiroki Tanaka <hiroki.tnk@gmail.com>
2021-01-19 09:17:18 +0900

    commit

--- abc051_b.py
+++ abc051_b.py
@@ -1,13 +1,12 @@
 K,S = map(int,input().split())
 
 ans = 0
-flag = True
 for i in range(K+1):
     for j in range(K+1):
         X = i
         Y = j
         Z = S - (X+Y)
-        if Z >= 0 and Z <= K:
-            ans +=1
+    if Z >= 0 and Z <= K:
+        ans +=1
 
 print(ans)
